4aa8a87593ab1e23f355c64ceaf777fc232b7095
Paul Dumais <paul@unstate.ca>
2018-12-30 23:27:06 -0400

    Try using wrap-protect-origins again

--- src/main/cawala/server_components/middleware.clj
+++ src/main/cawala/server_components/middleware.clj
@@ -102,7 +102,7 @@
       (wrap-api "/api")
       server/wrap-transit-params
       server/wrap-transit-response
-      #_(server/wrap-protect-origins {:allow-when-origin-missing? false
+      (server/wrap-protect-origins {:allow-when-origin-missing? true
                                     :legal-origins              legal-origins})
       (wrap-html-routes)
       ;; If you want to set something like session store, you'd do it against
